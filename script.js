e3cd3af788c458ef758756267e52175555ced8a2
thetripmakers <radhika.reddy0511@gmail.com>
2026-04-30 14:58:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function enquire() {
-  alert("Redirecting to contact page...");
   window.location.href = "contact.html";
 }
 
 function submitForm() {
-  const name = document.getElementById("name").value;
-  alert("Thank you " + name + "! We will contact you soon.");
+  alert("Thank you! We will contact you soon.");
 }
